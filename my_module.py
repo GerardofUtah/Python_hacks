ec0f2d465d042565835086f4b1b2983e90d22fd8
Leibowitz <93753040+GerardofUtah@users.noreply.github.com>
2021-11-05 04:48:41 +0100

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  5 04:11:16 2021
+
+MODULE my_module
+
+@author: user1
+"""
+
+def format_list_scientific(listtoformat,precision):
+    for i in range(len(listtoformat)):
+        listtoformat[i] = format(listtoformat[i],'.'+str(precision)+'E')
